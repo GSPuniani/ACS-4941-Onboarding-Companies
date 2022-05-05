a43023cab49a251e2c94aa3d9ca8e4b221b18fa0
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-05 14:02:27 -0700

    Added other problems and recursion problems

---
+++ Recursion/problem1.py
@@ -0,0 +1,6 @@
+"""
+Problem:
+Given a sorted array of strings, write a recursive function 
+to find the index of the first and last occurrence of a target string. 
+If the target string is not found in the array, report that.
+"""
---
+++ Recursion/problem2.py
@@ -0,0 +1,20 @@
+"""
+Problem:
+Given a string of digits 2 to 9 a user has pressed on a T9 “old school” telephone keypad, 
+return a list of all letter combinations they could have been trying to type on the keypad.
+"""
+
+"""
+Strategy: Save each digit as a key and a list of corresponding letters as a value in a dictionary, 
+then recursively iterate through all possible combos"""
+
+KEYPAD_DICT = {
+  2: ["a", "b", "c"],
+  3: ["d", "e", "f"],
+  4: ["g", "h", "i"],
+  5: ["j", "k", "l"],
+  6: ["m", "n", "o"],
+  7: ["p", "q", "r", "s"],
+  8: ["t", "u", "v"],
+  9: ["w", "x", "y", "z"]
+}
