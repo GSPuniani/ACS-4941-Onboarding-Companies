02e6b1b07d508742043246242f07064acf74268f
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-12 01:20:22 -0700

    Finished recursion problems

--- Recursion/problem1.py
+++ Recursion/problem1.py
@@ -4,3 +4,43 @@ Given a sorted array of strings, write a recursive function
 to find the index of the first and last occurrence of a target string. 
 If the target string is not found in the array, report that.
 """
+
+def binary_search_range(strings, target, left=0, right=0):
+    # Declare left and right pointers
+    if right == 0:
+        right = len(strings) - 1
+    # Declare list to hold index range of target
+    target_range = []
+        
+    mid = int((right + left) // 2)
+    
+    if strings[mid] == target:
+        target_range.append(mid)
+        # Check the right and left of the target at mid
+        other_mid = mid
+        while mid in range(len(strings)):
+            if strings[mid + 1] == target:
+                target_range.append(mid + 1)
+                mid += 1
+            else:
+                break
+        while other_mid in range(len(strings)):
+            if strings[other_mid - 1] == target:
+                target_range.append(other_mid - 1)
+                other_mid -= 1
+            else:
+                break
+        # Reshape target_from list of indices to actual range
+        target_range = [min(target_range), max(target_range)]
+        return target_range
+    # Use recursive calls
+    elif strings[mid] < target:
+        binary_search_range(strings, target, mid + 1, right)
+    else:
+        binary_search_range(strings, target, left, mid - 1)
+            
+    # If target not found, return -1
+    return -1
+
+# Output: [7, 10]
+print(binary_search_range(["Adriana", "Adriana", "Alan", "Alan", "Alan", "Alan", "Alan", "Braus", "Braus", "Braus", "Braus", "Dan", "Dan", "Dan", "Dan", "Dan", "Dani", "Dani", "Jess", "Meredith", "Milad", "Milad", "Mitchell", "Mitchell", "Mitchell", "Mitchell"], "Braus"))
--- Recursion/problem2.py
+++ Recursion/problem2.py
@@ -6,18 +6,29 @@ return a list of all letter combinations they could have been trying to type on 
 
 """
 Strategy: Save each digit as a key and a list of corresponding letters as a value in a dictionary, 
-then recursively iterate through all possible combos"""
+then iterate through all possible combos"""
+
+# Not technically recursive
+
+from itertools import product
 
 KEYPAD_DICT = {
-  "2": ["a", "b", "c"],
-  "3": ["d", "e", "f"],
-  "4": ["g", "h", "i"],
-  "5": ["j", "k", "l"],
-  "6": ["m", "n", "o"],
-  "7": ["p", "q", "r", "s"],
-  "8": ["t", "u", "v"],
-  "9": ["w", "x", "y", "z"]
+	"2": ["a", "b", "c"],
+	"3": ["d", "e", "f"],
+	"4": ["g", "h", "i"],
+	"5": ["j", "k", "l"],
+	"6": ["m", "n", "o"],
+	"7": ["p", "q", "r", "s"],
+	"8": ["t", "u", "v"],
+	"9": ["w", "x", "y", "z"]
 }
 
 def t9_letters(digits_str):
-  return 
+	# Return all possible Cartesian products based on corresponding lists for given input digits
+	cartesian_product = []
+	for i in range(len(digits_str) - 1):
+		cartesian_product.append(set(product(KEYPAD_DICT[digits_str[i]], KEYPAD_DICT[digits_str[i+1]])))
+
+	return cartesian_product
+
+print(t9_letters("23")) # Output: [{('c', 'f'), ('b', 'e'), ('a', 'd'), ('c', 'e'), ('b', 'f'), ('a', 'e'), ('b', 'd'), ('a', 'f'), ('c', 'd')}]
