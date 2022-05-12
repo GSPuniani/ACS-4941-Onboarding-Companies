7da73107dc61f2f20106cef9af6d5fe5f8d2bde4
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-11 22:10:58 -0700

    Finished two linked list problems

--- Linked_Lists/problem2.py
+++ Linked_Lists/problem2.py
@@ -3,3 +3,46 @@ Problem:
 Given a singly-linked list, rearrange the nodes by interleaving the 
 first half of the linked list with the second half.
 """
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        # Iterate with fast pointer and slow pointer to reach end and middle
+        if not head or not head.next:
+            return
+        slow = head
+        fast = head.next
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        slow = slow.next
+        if fast.next:
+            fast = fast.next
+            
+        # Reverse the second half of the list
+        prev = slow
+        curr = slow.next
+        prev.next = None
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+            
+        # Stitch together the first half and reversed second half
+        fast_front = head
+        while fast.next:
+            tmp1 = fast_front.next
+            tmp2 = fast.next
+            fast_front.next = fast
+            fast.next = tmp1
+            fast_front = tmp1
+            fast = tmp2
--- Linked_Lists/problem3.py
+++ Linked_Lists/problem3.py
@@ -2,3 +2,34 @@
 Problem:
 Rotate a given singly-linked list counter-clockwise by k nodes, where k is a given integer.
 """
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Return head if empty or k is 0
+        if not head or not head.next or k == 0:
+            return head
+        
+        # Keep count from traversal
+        count = 1
+        p = head
+        while p.next:
+            count += 1
+            p = p.next
+        p.next = head
+        # Modify k based on number of full cycles
+        k = count - (k % count)
+        
+        while k > 0:
+            k -= 1
+            p = p.next
+        head = p.next
+        p.next = None
+        
+        return head
+        
