234e73eeaa4adf752be7b186981266d81edacb27
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-12 00:47:03 -0700

    Finished all trees problems

--- Trees/bst_problem1.py
+++ Trees/bst_problem1.py
@@ -5,3 +5,43 @@ The range of the 'key' will be the first and last position of the 'key' in the a
 Write a function to return the range of the 'key'. If the 'key' is not present return [-1, -1].
 """
 
+def binary_search_range(nums, key):
+    # Declare left and right pointers
+    left = 0
+    right = len(nums) - 1
+    # Declare list to hold index range of key
+    key_range = []
+    while(left <= right):
+        mid = int((right + left) // 2)
+        
+        if nums[mid] ==  key:
+            key_range.append(mid)
+            # Check the right and left of the key at mid
+            target = mid
+            while mid in range(len(nums)):
+                if nums[mid + 1] == key:
+                    key_range.append(mid + 1)
+                    mid += 1
+                else:
+                    break
+            while target in range(len(nums)):
+                if nums[target - 1] == key:
+                    key_range.append(target - 1)
+                    target -= 1
+                else:
+                    break
+            # Reshape key_from list of indices to actual range
+            key_range = [min(key_range), max(key_range)]
+            return key_range
+        elif nums[mid] < key:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    # If key not found, return [-1, -1]
+    return [-1, -1]        
+
+
+print(binary_search_range([4, 6, 6, 6, 9], key = 6)) # Output: [1, 3]
+print(binary_search_range([1, 3, 8, 10, 15], key = 10)) # Output: [3, 3]
+print(binary_search_range([1, 3, 8, 10, 15], key = 12)) # Output: [-1, -1]
--- Trees/bst_problem2.py
+++ Trees/bst_problem2.py
@@ -4,3 +4,26 @@ Find the maximum value in a given Bitonic array.
 An array is considered bitonic if it is monotonically increasing and then monotonically decreasing. 
 Monotonically increasing or decreasing means that for any index i in the array arr[i] != arr[i+1].
 """
+
+def bitonic_max(nums):
+    # Declare left and right pointers
+    left = 0
+    right = len(nums) - 1
+
+    # Use similar logic as binary search
+    while(left <= right):
+        mid = int((right + left) // 2)
+        
+        # The max will be greater than both of its neighbors
+        if nums[mid] >= nums[mid + 1] and nums[mid] >= nums[mid - 1]:
+            return nums[mid]
+        # Reassign left pointer if values are increasing
+        elif nums[mid + 1] >= nums[mid]:
+            left = mid + 1
+        # Reassign right pointer if values are increasing
+        else:
+            right = mid - 1
+                    
+
+
+print(bitonic_max([1, 3, 8, 12, 4, 2])) # Output: 12
