704602c72c9d36cd97067a591ef3ecd8827d8515
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-11 22:44:16 -0700

    Finished linked list problems

--- Linked_Lists/problem1_new.py
+++ Linked_Lists/problem1_new.py
@@ -3,3 +3,32 @@ Problem:
 New: Given a singly-linked list and an integer k, find the value in the kth-to-last node.
 """
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        # Set slow and fast pointers to head node
+        slow = fast = head
+        
+        # Move fast pointer over by n places
+        while n > 0:
+            fast = fast.next
+            n -= 1
+			
+        # If n is size of linked list, remove head
+        if fast == None:
+            return head.next
+        
+        # Move fast and slow pointers by 1 
+        # Use fast and slow difference to locate target
+        while fast:
+            # Reassign slow's next pointer to delete target
+            if fast.next == None:
+                slow.next = slow.next.next
+                return head
+            fast = fast.next
+            slow = slow.next
--- Linked_Lists/problem4.py
+++ Linked_Lists/problem4.py
@@ -3,3 +3,42 @@ Problem:
 Given an array of k singly-linked lists, each of whose values are in sorted order, 
 combine all nodes (do not create new nodes) into one singly-linked list with all values in order.
 """
+
+# Not sure how to do this without creating new nodes
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Return empty node if list is empty or contains only None
+        if lists == [] or (len(lists) == 1 and lists[0] == None):
+            return ListNode('', None)
+        
+        # Traverse the linked lists and add each value to a list
+        n = 0
+        lst = []
+        for n in lists:
+            while n != None:
+                lst.append(n.val)
+                n = n.next
+        
+        # If list exists, sort it and assign first element to head
+        if lst:
+            lst.sort()
+            prev, cur = ListNode(lst[0], None), None
+            head = prev
+        else:
+            return ListNode('', None)
+        
+        # Add sorted elements from list to return linked list
+        for t in lst[1:]:
+            cur = ListNode(t, None)
+            prev.next = cur
+            prev = prev.next
+            
+        return head
+        
